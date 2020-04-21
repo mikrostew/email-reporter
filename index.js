9724ad5dd382dc2f07889e5d1116c2e9f66b84e2
Michael Stewart <mikrostew@gmail.com>
2020-04-20 23:43:23 -0700

    send email using config

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+const { getConfig } = require('./lib/get-config');
 const { promptForPassword } = require('./lib/prompt-for-password');
+const { sendEmail } = require('./lib/send-email');
 
-promptForPassword().then(password => {
-  console.log('Password is: ' + password);
-});
+// for testing while I develop this
+async function main() {
+  const config = await getConfig();
+  // TODO: validate that config
+  // NOTE: can enable this for debugging (or log it)
+  console.log(config);
+  const password = await promptForPassword(config.smtp.user);
+
+  const subject = "Hello #2 ✔";
+  const text = "Second email";
+  const html = "This is the <b>2nd</b> email I've sent from this script.";
+  await sendEmail(subject, text, html, config, password);
+}
+
+
+main().catch(console.error);
